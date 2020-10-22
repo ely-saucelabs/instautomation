aa15ed128ed44feda255eb7de6fd980d509a4d85
Ely Flores <ely.flores@saucelabs.com>
2020-10-22 23:12:07 +0200

    Add Product Test to add a photo

--- test/system/photos_test.rb
+++ test/system/photos_test.rb
@@ -7,4 +7,15 @@ class PhotosTest < ApplicationSystemTestCase
     assert_selector "a", text: "Instautomation"
     assert_selector "a", text: "Home"
   end
+
+  test "can add a photo" do
+    visit photos_url
+
+    click_on "New Photo"
+    fill_in "photo[description]", with: "Earthrise"
+    attach_file "photo[picture]", file_fixture('picture.png')
+    click_on "Add"
+
+    assert_selector "p", text: "Earthrise"
+  end
 end
