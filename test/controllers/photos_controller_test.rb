de3f9d5f1058fa995c708d52c7d2cd54adeef43f
Ely Flores <ely.flores@saucelabs.com>
2020-10-22 23:42:50 +0200

    Add Developer Tests to add a photo

--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -1,8 +1,30 @@
 require 'test_helper'
 
 class PhotosControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @photo = photos(:one)
+  end
+
   test "should get index" do
     get photos_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_photo_url
+    assert_response :success
+  end
+
+  test "should create new photo" do
+    assert_difference('Photo.count') do
+      post photos_url, params: { photo: { description: @photo.description, picture: @photo.picture } }
+    end
+
+    assert_redirected_to photo_url(Photo.last)
+  end
+
+  test "should show photo" do
+    get photo_url(@photo)
     assert_response :success
   end
 
