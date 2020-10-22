dae6165ca14cf9930f4965a0e707a534034eb95f
Ely Flores <ely.flores@saucelabs.com>
2020-10-23 00:04:59 +0200

    Update System Tests Config

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
+Webdrivers::Geckodriver.update
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # Selenium driving Firefox in a headless configuration
   driven_by :selenium_headless, screen_size: [1400, 1400]
