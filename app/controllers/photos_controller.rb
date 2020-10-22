813cf5ffd7b74529b37465f14fb08885abac97e5
Ely Flores <ely.flores@saucelabs.com>
2020-10-22 23:54:46 +0200

    Implement feature: Add a photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,38 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, only: :show
+
   def index
+  end
+
+  def new 
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to @photo, notice: 'Your photo was successfully created.' }
+        format.json { render :show, status: :created, location: @photo }
+      else
+        format.html { render :new }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show 
+    @photo
+  end
+
+  private
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
+
+  #  Only allow certain parameters through.
+  def photo_params
+    params.require(:photo).permit(:description, :picture)
   end
 end
