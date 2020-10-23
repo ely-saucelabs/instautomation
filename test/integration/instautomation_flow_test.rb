6af98595e2b4ee774607a4b6dfd025278c4f88b4
Ely Flores <ely.flores@saucelabs.com>
2020-10-23 17:16:17 +0200

    Add Developer Test to see all photos in index

--- test/integration/instautomation_flow_test.rb
+++ test/integration/instautomation_flow_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class InstautomationFlowTest < ActionDispatch::IntegrationTest
+  setup do
+    @photo_one = photos(:one)
+    @photo_two = photos(:two)
+  end
+
   test "can see the index page" do
     get "/"
 
@@ -13,5 +18,15 @@ class InstautomationFlowTest < ActionDispatch::IntegrationTest
     get "/pages/about"
 
     assert_select "h1", "About"
+  end
+
+  test "can see added photos in the index page" do 
+    post "/photos", params: { photo: { description: @photo_one.description, picture: @photo_one.picture } }
+    post "/photos", params: { photo: { description: @photo_two.description, picture: @photo_two.picture } }
+
+    get "/"
+
+    assert_select "p", text: @photo_one.description
+    assert_select "p", text: @photo_two.description
   end
 end
